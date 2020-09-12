450e0a4f23e1db76dabdd35ed142b7937043b74a
VACHARA VIRIYAKUL <63010871@KMITL.AC.TH>
2020-09-12 15:50:53 +0700

    Add project files.

---
+++ Act1/Act1.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<iostream>
+char tolower(char*p);
+char toupper(char*p);
+
+
+int main()
+{
+	char* p, str[50];
+	p = str;
+	scanf("%[^\n]s", str);
+	while (*p != '\0') {
+		if (*p >= 'a' && *p <= 'z')
+		{
+			printf("%c", toupper(*p));
+		}
+		else if (*p >= 'A' && *p <= 'Z')
+		{
+			printf("%c", tolower(*p));
+		}
+		p++;
+	}
+	return 0;
+}
+
+char toupper(char* p)
+{
+	*p -= 32;
+	return *p;
+}
+
+char tolower(char* p)
+{
+	*p += 32;
+	return *p;
+}
